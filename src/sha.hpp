466d1a05a7de68780f15cd9cd37725fb463bb681
mojyack <mojyack@gmail.com>
2024-10-07 10:14:27 +0900

    sha: fix argument name

--- src/sha.hpp
+++ src/sha.hpp
@@ -4,5 +4,5 @@
 
 namespace crypto::sha {
 auto calc_sha1(std::span<const std::byte> data) -> std::array<std::byte, 20>;
-auto calc_sha256(std::span<const std::byte> str) -> std::array<std::byte, 32>;
+auto calc_sha256(std::span<const std::byte> data) -> std::array<std::byte, 32>;
 } // namespace crypto::sha
